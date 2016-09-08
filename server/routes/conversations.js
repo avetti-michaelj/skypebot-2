94ee7e4d64fbdf84c801373220210d71fc723896
m-r-joyce <200284329@student.georgianc.on.ca>
2016-09-08 16:40:27 -0400

    Parentheses

--- server/routes/conversations.js
+++ server/routes/conversations.js
@@ -5,7 +5,15 @@ var Conversations = require('../models/conversation');
 
 /* GET conversations listing. */
 router.get('/', function(req, res, next) {
-  res.json({Conversations});
+  Conversations.find(function (err, conversations) {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        }
+        else {
+          res.json({conversations});
+        }
+  });
 });
 
 router.post('/', function(req, res, next) {
